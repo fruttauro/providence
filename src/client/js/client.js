4ca408acc2852aa65bc30ef621899784b023adc1
Daniel Fruttauro <danielfruttauro@driveworks.co.uk>
2019-05-14 17:27:20 +0100

    thats better

--- src/client/js/client.js
+++ src/client/js/client.js
@@ -1,6 +1,17 @@
 const socket = new WebSocket(`wss://localhost:1337`);
 
 socket.onerror = () => console.log(error);
-socket.onopen = () => socket.send("Hey");
+socket.onopen = () => sendSocketMessage("Hey");
 socket.onclose = () => console.log("Web sockets go bye bye.");
 socket.onmessage = (message) => console.log(message);
+
+function sendSocketMessage(message) {
+
+    if (socket.readyState !== socket.OPEN) {
+        console.log("Web Socket connection is not open. Message could not be sent.");
+
+        return;
+    }
+
+    socket.send(message);
+}

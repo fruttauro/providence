3ab5665145168eaaa7a030b63e516ee7b76ccaa5
Daniel Fruttauro <danielfruttauro@driveworks.co.uk>
2019-05-14 17:22:28 +0100

    Tidied client.js

--- src/client/js/client.js
+++ src/client/js/client.js
@@ -1,11 +1,6 @@
-(async () => {
-    const socket = new WebSocket(`wss://localhost:1337`);
+const socket = new WebSocket(`wss://localhost:1337`);
 
-    socket.onerror = () => console.log(error);
-
-    socket.onopen = () => socket.send("Hey");
-
-    socket.onclose = () => console.log("Web sockets go bye bye.");
-
-    socket.onmessage = (message) => console.log(message);
-})();
+socket.onerror = () => console.log(error);
+socket.onopen = () => socket.send("Hey");
+socket.onclose = () => console.log("Web sockets go bye bye.");
+socket.onmessage = (message) => console.log(message);

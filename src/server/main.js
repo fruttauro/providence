60b0ea037ba89c5640f5a88a7d38f26f30725b1d
Daniel Fruttauro <danielfruttauro@driveworks.co.uk>
2019-05-14 16:11:03 +0100

    Added nedb to server so that we have a persistent db to store subscriptions in

--- src/server/main.js
+++ src/server/main.js
@@ -1,22 +1,26 @@
 const app = require("express")();
-const https = require("https");
 const fs = require("fs-extra");
+const https = require("https");
+const dataStore = require("nedb");
 
 const PORT_NO = 1337;
-const SECRETS_DIR = "./settings/secrets/";
+const SECRETS_DIR = "./settings/secrets";
+const STORAGE_DIR = "./storage";
+
+const pushSubscriptionsDataStore = new dataStore({ filename: `${STORAGE_DIR}/pushSubscriptions.db`, autoload: true });
 
 (async () => {
-    const SSL_KEY = await fs.readFile(`${SECRETS_DIR}localhost.key`);
-    const SSL_CERT = await fs.readFile(`${SECRETS_DIR}localhost.cer`);
+    const SSL_KEY = await fs.readFile(`${SECRETS_DIR}/localhost.key`);
+    const SSL_CERT = await fs.readFile(`${SECRETS_DIR}/localhost.cer`);
 
     app.get("/", (_req, res) => {
         res.send("Hello World!");
     });
 
-    https.createServer({
-        key: SSL_KEY,
-        cert: SSL_CERT,
-        passphrase: "Potatoes"
-    }, app)
-        .listen(PORT_NO);
+    https.createServer(
+        {
+            key: SSL_KEY,
+            cert: SSL_CERT
+        },
+        app).listen(PORT_NO);
 })();

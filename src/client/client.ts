0a6c778bbac8bd8d78355acf82a1414fa4d6a63f
Daniel Fruttauro <danielfruttauro@driveworks.co.uk>
2019-05-15 17:00:11 +0100

    Basic system in place for prompting a user for push permissions and saving that subscription to a database. Also initialize a service worker.

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,26 @@
-const socket = new WebSocket(`wss://localhost:1337`);
+const PUBLIC_PUSH_KEY = "BPfbkLtT8zRVfuLNsHXbCh89238qFJjZewmHsPCTDLhbRTFavy1naX00H74sc85yOK72WeTTHNxa78ZLYfHuEdM";
+const API_BASE_URL = "https://ws-dev-danny:1337/api";
+
+const socket = new WebSocket(`wss://ws-dev-danny:1337`);
 
 socket.onerror = (error) => console.log(error);
-socket.onopen = () => sendSocketStringMessage("Hey");
+
+socket.onopen = async () => {
+
+    sendSocketStringMessage("Hey");
+
+    const registration = await registerServiceWorker();
+
+    if (registration !== null) {
+        const pushPermission = await askPermission();
+
+        if (pushPermission === "granted") {
+            const subscription = await subscribeUserToPush(registration);
+            postPushSubscription(subscription);
+        }
+    }
+};
+
 socket.onclose = () => console.log("Web sockets go bye bye.");
 socket.onmessage = (message) => console.log(message);
 
@@ -14,4 +33,70 @@ function sendSocketStringMessage(message: string) {
     }
 
     socket.send(message);
+}
+
+async function registerServiceWorker() {
+
+    if (!("serviceWorker" in navigator) || !("PushManager" in window)) {
+        console.log("This browser does not support service workers and/or the Push API.");
+
+        return null;
+    }
+
+    try {
+        const registration = await navigator.serviceWorker.register("client/serviceWorker.js");
+
+        return registration;
+    } catch (err) {
+
+        console.error("Unable to register service worker.", err);
+    }
+
+    return null;
+}
+
+async function askPermission() {
+    return Notification.requestPermission();
+}
+
+async function subscribeUserToPush(registration: ServiceWorkerRegistration) {
+
+    const existingSubscription = await registration.pushManager.getSubscription();
+
+    if (existingSubscription !== null) {
+        return existingSubscription;
+    }
+
+    const subscriptionOptions: PushSubscriptionOptionsInit = {
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(PUBLIC_PUSH_KEY)
+    };
+
+    return await registration.pushManager.subscribe(subscriptionOptions);
+}
+
+function urlBase64ToUint8Array(base64String: string) {
+    const padding = "=".repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/");
+    const rawData = window.atob(base64);
+
+    return Uint8Array.from([...rawData].map((char) => char.charCodeAt(0)));
+}
+
+async function postPushSubscription(subscription: PushSubscription) {
+    const response = await fetch(`${API_BASE_URL}/pushSubscription`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(subscription)
+    });
+
+    if (!response.ok) {
+        console.log("Server says no.");
+
+        return {};
+    }
+
+    return response.json();
 }
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,3 +1,5 @@
+import * as bodyParser from "body-parser";
+import * as cors from "cors";
 import * as express from "express";
 import * as fs from "fs-extra";
 import * as https from "https";
@@ -12,10 +14,16 @@ const STATIC_CONTENT_DIR = path.join(__dirname, "../../staticContent");
 const DIST_DIR = path.join(__dirname, "../");
 
 const app = express();
+
+// Preflight CORS for all routes
+app.options("*", cors());
+
+app.use(bodyParser.json());
 app.use(express.static(STATIC_CONTENT_DIR));
 app.use(express.static(DIST_DIR));
 
 const pushSubscriptionsDataStore = new dataStore({ filename: `${STORAGE_DIR}/pushSubscriptions.db`, autoload: true });
+pushSubscriptionsDataStore.ensureIndex({ fieldName: "endpoint", unique: true }, (error) => console.log(error));
 
 (async () => {
     const SSL_KEY = await fs.readFile(`${SECRETS_DIR}/localhost.key`);
@@ -28,15 +36,49 @@ const pushSubscriptionsDataStore = new dataStore({ filename: `${STORAGE_DIR}/pus
         res.sendFile(path.join(__dirname, `${STATIC_CONTENT_DIR}/index.html`));
     });
 
+    app.post("/api/pushSubscription", cors(), (req, res) => {
+        const pushSubscriptionData = <PushSubscription>req.body;
+
+        if (!pushSubscriptionData || !pushSubscriptionData.endpoint) {
+            res.status(400);
+            res.setHeader("Content-Type", "application/json");
+            res.send(JSON.stringify({ data: { success: false, message: "The push subscription request was invalid. " } }));
+        }
+
+        pushSubscriptionsDataStore.insert(pushSubscriptionData, (error, _document) => {
+            if (error) {
+
+                // Thanks sucky typings
+                if ((<any>error).errorType !== "uniqueViolated") {
+
+                    res.status(500);
+                    res.setHeader("Content-Type", "application/json");
+                    res.send(JSON.stringify({
+                        data: {
+                            success: false,
+                            message: "The subscription was received but we were unable to save it to our database."
+                        }
+                    }));
+
+                    return;
+                }
+            }
+
+            res.status(200);
+            res.setHeader("Content-Type", "application/json");
+            res.send(JSON.stringify({ data: { success: true, message: "The subscription was added to our database." } }));
+        });
+    });
+
     wss.on("connection", (ws) => {
 
         ws.on("message", (message) => {
-            console.log(message);
             ws.send(`What did you just say to me?! "${message}" is offensive round these parts.`);
         });
 
         ws.send("Hello, web sockets rock!");
     });
-    console.log(pushSubscriptionsDataStore.persistence);
+
     server.listen(PORT_NO);
+    console.log(`Big Brother Is Watching at Port ${PORT_NO}`);
 })();

1a37add7cfcb546242e553d9e1911e9b7c8bed21
Daniel Fruttauro <danielfruttauro@driveworks.co.uk>
2019-05-15 09:58:11 +0100

    Added TypeScript to all of the things. Changed directory structure to account for buildy stuffs.

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,11 +1,11 @@
 const socket = new WebSocket(`wss://localhost:1337`);
 
-socket.onerror = () => console.log(error);
-socket.onopen = () => sendSocketMessage("Hey");
+socket.onerror = (error) => console.log(error);
+socket.onopen = () => sendSocketStringMessage("Hey");
 socket.onclose = () => console.log("Web sockets go bye bye.");
 socket.onmessage = (message) => console.log(message);
 
-function sendSocketMessage(message) {
+function sendSocketStringMessage(message: string) {
 
     if (socket.readyState !== socket.OPEN) {
         console.log("Web Socket connection is not open. Message could not be sent.");
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,17 +1,19 @@
-const dataStore = require("nedb");
-const express = require("express");
-const fs = require("fs-extra");
-const https = require("https");
-const path = require("path");
-const webSocket = require("ws");
+import * as express from "express";
+import * as fs from "fs-extra";
+import * as https from "https";
+import * as dataStore from "nedb";
+import * as path from "path";
+import * as webSocket from "ws";
 
 const PORT_NO = 1337;
 const SECRETS_DIR = "./settings/secrets";
 const STORAGE_DIR = "./storage";
-const CLIENT_DIR = path.join(__dirname, "../client");
+const STATIC_CONTENT_DIR = path.join(__dirname, "../../staticContent");
+const DIST_DIR = path.join(__dirname, "../");
 
 const app = express();
-app.use(express.static(CLIENT_DIR));
+app.use(express.static(STATIC_CONTENT_DIR));
+app.use(express.static(DIST_DIR));
 
 const pushSubscriptionsDataStore = new dataStore({ filename: `${STORAGE_DIR}/pushSubscriptions.db`, autoload: true });
 
@@ -23,18 +25,18 @@ const pushSubscriptionsDataStore = new dataStore({ filename: `${STORAGE_DIR}/pus
     const wss = new webSocket.Server({ server });
 
     app.get("/", (_req, res) => {
-        res.sendFile(path.join(__dirname, `${CLIENT_DIR}/index.html`));
+        res.sendFile(path.join(__dirname, `${STATIC_CONTENT_DIR}/index.html`));
     });
 
     wss.on("connection", (ws) => {
 
         ws.on("message", (message) => {
             console.log(message);
-            ws.send(`What did you just say to me?! "${message}" is offensive round these parts.`)
+            ws.send(`What did you just say to me?! "${message}" is offensive round these parts.`);
         });
 
         ws.send("Hello, web sockets rock!");
     });
-
+    console.log(pushSubscriptionsDataStore.persistence);
     server.listen(PORT_NO);
-})(); 
+})();
